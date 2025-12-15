657533c3e371015b53b1d9af3e5ab502bf57ac42
kristiyan <bazzana87@gmail.com>
2025-12-15 13:47:00 +0200

    lift cart up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,17 +1,25 @@
+import axios from "axios";
 import { Routes, Route } from "react-router";
-import "./App.css";
+import { useEffect, useState } from "react";
 import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import { NotFoundPage } from "./pages/NotFoundPage.jsx";
+import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
   return (
     <>
       <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route index element={<HomePage cart={cart} />} />
+        <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/tracking" element={<TrackingPage />} />
         <Route path="*" element={<NotFoundPage />} />
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,16 +4,11 @@ import { Header } from "../components/Header.jsx";
 import CheckmarkIcon from "../assets/images/icons/checkmark.png";
 import "./HomePage.css";
 
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
   useEffect(() => {
     axios.get("/api/products").then((response) => {
       setProducts(response.data);
-    });
-
-    axios.get("http://localhost:3000/api/cart-items").then((response) => {
-      setCart(response.data);
     });
   }, []);
 
