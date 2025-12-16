30cd4fdf9b19c8821185050eebef416e358b107b
kristiyan <bazzana87@gmail.com>
2025-12-16 22:27:23 +0200

    generate HTML for orders grid

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,9 +1,7 @@
 import axios from "axios";
-import dayjs from "dayjs";
 import { Header } from "../../components/Header.jsx";
 import { useEffect, useState } from "react";
-import { formatMoney } from "../../utils/money.js";
-import { OrdersGridDetails } from "./OrdersGridDetails.jsx";
+import { OrdersGrid } from "./OrdersGrid.jsx";
 import "./OrdersPage.css";
 
 export function OrdersPage({ cart }) {
@@ -22,34 +20,7 @@ export function OrdersPage({ cart }) {
 
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
-
-        <div className="orders-grid">
-          {orders.map((order) => {
-            return (
-              <div key={order.id} className="order-container">
-                <div className="order-header">
-                  <div className="order-header-left-section">
-                    <div className="order-date">
-                      <div className="order-header-label">Order Placed:</div>
-                      <div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
-                    </div>
-                    <div className="order-total">
-                      <div className="order-header-label">Total:</div>
-                      <div>{formatMoney(order.totalCostCents)}</div>
-                    </div>
-                  </div>
-
-                  <div className="order-header-right-section">
-                    <div className="order-header-label">Order ID:</div>
-                    <div>{order.id}</div>
-                  </div>
-                </div>
-
-                <OrdersGridDetails products={order.products} />
-              </div>
-            );
-          })}
-        </div>
+        <OrdersGrid orders={orders} />
       </div>
     </>
   );
