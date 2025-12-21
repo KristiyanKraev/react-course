ceefae94304446191290e6cd552c45dddd7d12be
kristiyan <bazzana87@gmail.com>
2025-12-21 20:38:51 +0200

    test clicking Add to cart button

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,6 +1,10 @@
 import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { Product } from "./Product";
+import axios from "axios";
+import userEvent from "@testing-library/user-event";
+
+vi.mock("axios");
 describe("Product component", () => {
   it("display product details correctly", () => {
     const product = {
@@ -21,19 +25,46 @@ describe("Product component", () => {
     expect(
       screen.getByText("Black and Gray Athletic Cotton Socks - 6 Pairs")
     ).toBeInTheDocument();
+
+    expect(screen.getByText("$10.90")).toBeInTheDocument();
+
+    expect(screen.getByTestId("product-image")).toHaveAttribute(
+      "src",
+      "images/products/athletic-cotton-socks-6-pairs.jpg"
+    );
+
+    expect(screen.getByTestId("product-rating-stars-image")).toHaveAttribute(
+      "src",
+      "images/ratings/rating-45.png"
+    );
+
+    expect(screen.getByText("87")).toBeInTheDocument();
   });
 
-  expect(screen.getByText("$10.90")).toBeInTheDocument();
+  it("adds a product to the cart", async () => {
+    const product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"],
+    };
 
-  expect(screen.getByTestId("product-image")).toHaveAttribute(
-    "src",
-    "images/products/athletic-cotton-socks-6-pairs.jpg"
-  );
+    const loadCart = vi.fn();
+    render(<Product product={product} loadCart={loadCart} />);
 
-  expect(screen.getByTestId("product-rating-stars-image")).toHaveAttribute(
-    "src",
-    "images/ratings/rating-45.png"
-  );
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByTestId("add-to-cart-button");
+    await user.click(addToCartButton);
 
-  expect(screen.getByText("87")).toBeInTheDocument();
+    expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+    });
+    expect(loadCart).toHaveBeenCalled();
+  });
 });
