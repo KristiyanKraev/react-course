fa5825bc61649e4fac991dab0736e015e67aaf4a
kristiyan <bazzana87@gmail.com>
2025-12-21 12:17:14 +0200

    create update delivery feature

--- ecommerce-project/src/pages/checkout/DeliveryOptions.jsx
+++ ecommerce-project/src/pages/checkout/DeliveryOptions.jsx
@@ -1,7 +1,8 @@
 import dayjs from "dayjs";
+import axios from "axios";
 import { formatMoney } from "../../utils/money";
 
-export function DeliveryOptions({ deliveryOptions, cartItem }) {
+export function DeliveryOptions({ deliveryOptions, cartItem, loadCart }) {
   return (
     <div className="delivery-options">
       <div className="delivery-options-title">Choose a delivery option:</div>
@@ -12,11 +13,23 @@ export function DeliveryOptions({ deliveryOptions, cartItem }) {
           priceString = `${formatMoney(option.priceCents)} - Shipping`;
         }
 
+        const updateDeliveryOption = async () => {
+          await axios.put(`/api/cart-items/${cartItem.productId}`, {
+            deliveryOptionId: option.id,
+          });
+          await loadCart();
+        };
+
         return (
-          <div key={option.id} className="delivery-option">
+          <div
+            key={option.id}
+            className="delivery-option"
+            onClick={updateDeliveryOption}
+          >
             <input
               type="radio"
               checked={option.id === cartItem.deliveryOptionId}
+              onChange={() => {}}
               className="delivery-option-input"
               name={`delivery-option-${cartItem.productId}`}
             />
